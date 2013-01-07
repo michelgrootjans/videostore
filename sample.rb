b54338e0a9ff5229bfe338d894be0b28537db442
Michel Grootjans <michel.grootjans@gmail.com>
2013-01-07 09:18:53 +0100

    fixed typo

--- sample.rb
+++ sample.rb
@@ -5,7 +5,7 @@ require_relative "lib/movie"
 mad_max = Movie.new("Mad Max", Movie::REGULAR)
 poltergeist = Movie.new("Poltergeist", Movie::REGULAR)
 the_hobbit = Movie.new("The Hobbit", Movie::NEW_RELEASE)
-bambi = Movie.new("Babmi", Movie::CHILDRENS)
+bambi = Movie.new("Bambi", Movie::CHILDRENS)
 
 customer = Customer.new "Martin Fowler"
 customer.add_rental(Rental.new(mad_max, 3))
