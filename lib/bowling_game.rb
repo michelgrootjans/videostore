6b591e969805cdd231e3a45b089bfd4c6f47acab
Michel Grootjans <michel.grootjans@gmail.com>
2016-07-18 22:47:38 +0200

    added a simple test

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -2,4 +2,7 @@ class BowlingGame
   def score
     0
   end
+
+  def roll(pins)
+  end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -4,4 +4,9 @@ require 'bowling_game'
 describe BowlingGame do
   subject(:game){ BowlingGame.new }
   it { expect(game.score).to eq 0 }
+
+  context 'rolling 0' do
+    before { game.roll(0) }
+    it { expect(game.score).to eq 1 }
+  end
 end
